8db181c4e7a18bc11b64f3e1414358ca75e88337
anshu <anshubullettiwari@gmail.com>
2021-01-30 19:14:11 +0530

    Login Commit

---
+++ app/src/main/java/com/anshutiwari/hostelapp/Admin/AdminLoginActivity.java
@@ -0,0 +1,16 @@
+package com.anshutiwari.hostelapp.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.anshutiwari.hostelapp.R;
+
+public class AdminLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_login);
+    }
+}
