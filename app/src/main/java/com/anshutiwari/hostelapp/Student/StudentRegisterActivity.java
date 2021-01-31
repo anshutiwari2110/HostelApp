dbb487cdfc858f7f51368d9ff89518ff425e57d8
anshu <anshubullettiwari@gmail.com>
2021-01-31 14:37:31 +0530

    Register Commit

---
+++ app/src/main/java/com/anshutiwari/hostelapp/Student/StudentRegisterActivity.java
@@ -0,0 +1,16 @@
+package com.anshutiwari.hostelapp.Student;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.anshutiwari.hostelapp.R;
+
+public class StudentRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_register);
+    }
+}
